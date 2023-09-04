8ac224e304fcdb54c6edb639db85255634587442
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-09-04 18:33:05 -0400

    perf: Use RuntimeHelpers to elide array allocations

--- RemoraPolyfillCache/ExpressionBuilder.cs
+++ RemoraPolyfillCache/ExpressionBuilder.cs
@@ -7,7 +7,11 @@ namespace RemoraPolyfillCache;
 
 public static class ExpressionBuilder
 {
+    private static readonly MethodInfo _getUnitializedObject = typeof(RuntimeHelpers).GetMethod
+        (nameof(RuntimeHelpers.GetUninitializedObject), BindingFlags.Public | BindingFlags.Static)!;
+    
     internal static Dictionary<Type, Delegate> _lookupCache = new();
+    
     
     public static T Polyfill<T>(T old, T @new)
     {
@@ -64,6 +68,59 @@ public static class ExpressionBuilder
         _lookupCache[typeInfo] = func;
 
         return func(GetProperties(old), GetProperties(@new));
+    }
+    
+    public static T PolyfillFast<T>(T old, T @new)
+    {
+        if (_lookupCache.ContainsKey(typeof(T)))
+        {
+            return Unsafe.As<Func<T, T, T>>(_lookupCache[typeof(T)])(old, @new);
+        }
+        
+        // Cached delegate unavailable; build one.
+        var typeInfo = typeof(T);
+        var properties = typeInfo.GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+        var setters = new Expression[properties.Length + 1];
+
+        var oldParam = Expression.Parameter(typeInfo, "old");
+        var newParam = Expression.Parameter(typeInfo, "new");
+
+        var instance = Expression.Convert(Expression.Call(_getUnitializedObject, Expression.Constant(typeInfo)), typeInfo);
+        
+        for (int i = 0; i < setters.Length - 1; i++)
+        {
+            var argumentType = properties[i].FieldType;
+
+            var left = Expression.Field(oldParam, properties[i]);
+            var right = Expression.Field(newParam, properties[i]);
+
+            Expression check;
+
+            if (argumentType.IsGenericType && argumentType.GetGenericTypeDefinition() == typeof(Optional<>))
+            {
+                // Check that the provider of the polyfill has a value for us. If it does, always update.
+                check = Expression.NotEqual(right, Expression.Default(argumentType));
+            }
+            else
+            {
+                check = Expression.NotEqual(left, right);
+            }
+
+            setters[i] = Expression.Assign(instance, Expression.Condition(check, right, left, argumentType));
+        }
+
+        setters[^1] = instance;
+
+        var block = Expression.Block(setters);
+        
+        var lambda = Expression.Lambda<Func<T, T, T>>(block, oldParam, newParam);
+
+        var func = lambda.Compile();
+
+        _lookupCache[typeInfo] = func;
+
+        return func(old, @new);
     }
 
     private static object?[] GetProperties<T>(T obj)
